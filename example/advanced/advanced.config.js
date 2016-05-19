c085897c0453b08b70d854a5835051dc6eba0baf
johncomposed <johncomposed@gmail.com>
2016-05-19 18:50:27 -0400

    Removed my personal research and added examples boilerplate

---
+++ example/advanced/advanced.config.js
@@ -0,0 +1,9 @@
+'use strict';
+/*
+For details about this file, wait until the v1 release!
+
+
+*/
+module.exports = function(utils, dir) {
+  // todo
+};
---
+++ example/default/default.config.js
@@ -0,0 +1,11 @@
+'use strict';
+/*
+A basic example
+
+For details about this file, wait until the v1 release!
+
+
+*/
+module.exports = function(utils, dir) {
+  // todo
+};
