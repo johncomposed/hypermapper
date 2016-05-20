c38dced3dfbfc24f06582496d7fb8816198b724a
johncomposed <johncomposed@gmail.com>
2016-05-20 03:42:41 -0400

    Turns out to fix reload i had to rewrite a lot of stuff

--- src/crawl.js
+++ src/crawl.js
@@ -1,44 +1,46 @@
 'use strict';
+const Crawler = require('simplecrawler').Crawler;
+const _ = require('lodash');
+const u = require('./utils');
+const defaultConfig = require('./config.js');
 
-module.exports = function(config, crawl_callback) {
-  const Crawler = require('simplecrawler').Crawler;
-  const _ = require('lodash');
+
+module.exports = class Crawl {
   
-  const myCrawler = new Crawler(config.domain);
+  constructor(configPath) {
+    const config = this.config = _.merge(defaultConfig, require(configPath)(u, __dirname)).crawl;
+    const crawler = this.crawler = new Crawler(config.domain);
 
-  // Defrost if set
-  if (config.defrost) { myCrawler.queue.defrost(config.defrost); }
+    // Defrost if set
+    if (config.defrost) { crawler.queue.defrost(config.defrost); }
 
-  // Set crawler config
-  _.assign(myCrawler, config.crawler);
+    // Set crawler config
+    _.merge(crawler, config.crawler);
 
-  // Set onConfig
-  _.forEach(config.on, (value, key) => myCrawler.on(key, value));
-  
-  // Custom fetch conditions
-  _.forEach(config.fetchConditions, (value, key) => myCrawler.addFetchCondition(value));
-  
-  // Callback on complete
-  myCrawler.on("complete", () => crawl_callback(config.domain));
+    // Set onConfig
+    _.forEach(config.on, (value, key) => crawler.on(key, value));
+    
+    // Custom fetch conditions
+    _.forEach(config.fetchConditions, (value, key) => crawler.addFetchCondition(value));
 
-
-  // Start Crawl
-  myCrawler.start();
-
-
-  // Freeze before kill
-  if (config.freeze) {
-    process.on("SIGINT", function() {
-      myCrawler.queue.freeze(config.freeze, function() {
+    // Callback on complete
+    crawler.on("complete", () => console.log("Complete crawling %s!", config.domain));
+    
+    // Freeze before kill
+    if (config.freeze) {
+      process.on("SIGINT", () => crawler.queue.freeze(config.freeze, function() {
         console.log("Frozen queue to %s", config.freeze);
         process.exit();
-      });
-    });
+      }));
+    }
+  }
+
+  start() {
+    console.log(this.config);
+    this.crawler.start();
   }
   
-  
-  
-  
-  
-  
+  getCrawler() {
+    return this.crawler;
+  }
 };
--- src/viz.js
+++ src/viz.js
@@ -1,58 +1,69 @@
 'use strict';
+const u = require('./utils');
+const _ = require('lodash');
+const express = require('express');
+const defaultConfig = require('./config.js');
 
-const Viz = module.exports = function(config, viz_callback) {
-  const u = require('./utils');
-  const _ = require('lodash');
-  const express = require('express');
-  const port = config.port;
+module.exports = class Viz {
   
-  const app = express();
+  constructor(configPath) {
+    this.configPath = configPath;
+    this.config = _.merge(defaultConfig, require(this.configPath)(u, __dirname)).viz;
+    this.app = express();
+  }
   
-  const libScripts = config.libs.map((c) => `<script type="text/javascript" src="${c}"></script>`).join('\n');
-  const page = `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <title>cytoscape</title>
-      ${libScripts}
-      <script type="text/javascript" src="./data.js"> </script>
-      <script type="text/javascript">
-        document.addEventListener("DOMContentLoaded", function(event) { 
-          (${config.script})(cytoscape, data)
-        });
-      </script>
-      <style>
-      body, #cy {
-          height: 100vh;
-          width: 100vw;
-          margin: 0;
-          padding: 0;
-      }
-      </style>
-    </head>
-    <body>
-      <div id="cy"></div>
-    </body>
-
-  </html>
-  `;
-
-  app.use('/public/', express.static(config.publicDir));
+  start() {
+    let port = this.config.port;
+    this.initRoutes();
+    this.server = this.app.listen(port, console.log("Visualization server started on port %s", port));
+  }
   
-  app.get('/', function (req, res) {
-    console.log("Loading index");
-    res.send(page);
-  });
-
-  app.get('/data.js', function (req, res) {
-    res.set('Content-Type', 'text/javascript');
-    config.data(function(err, data){
-      if (err) {console.log(err);}
-      
-      res.send(`var data = ${JSON.stringify(data)}`);
+  restart() {
+    delete require.cache[this.configPath];
+    this.config = _.merge(defaultConfig, require(this.configPath)(u, __dirname)).viz;
+    this.server.close();
+    this.start();
+    
+  }
+  
+  initRoutes() {
+    let styles = this.config.styles.map((c) => `<link type="text/css" rel="stylesheet" src="${c}"/>`).join('\n');
+    let libs = this.config.libs.map((c) => `<script type="text/javascript" src="${c}"></script>`).join('\n');
+    let page = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <title>Viz</title>
+          <style> body, #cy { height: 100vh; width: 100vw; margin: 0; padding: 0;} </style>
+          ${styles}
+        </head>
+        <body>
+          <div id="viz"></div>
+        </body>
+        ${libs}
+        <script type="text/javascript" src="./data.js"> </script>
+        <script type="text/javascript">
+          document.addEventListener("DOMContentLoaded", function(event) { 
+            (${this.config.script})(cytoscape, data)
+          });
+        </script>
+      </html>
+      `;
+    
+    this.app.use('/public/', express.static(this.config.publicDir));
+    
+    this.app.get('/data.js', function (req, res) {
+      res.set('Content-Type', 'text/javascript');
+      this.config.data(function(err, data){
+        if (err) {console.log(err);}
+        
+        res.send(`var data = ${JSON.stringify(data)}`);
+      });
     });
-  });
 
-  app.listen(port);
-  viz_callback(port);
+    this.app.get('/', function (req, res) {
+      console.log("Loading index");
+      res.send(page);
+    });
+  }
 };
